71725ef164966a77048a30c78f40ad1b28bb9256
ALO922 <jvegadelgado802@gmail.com>
2024-09-02 20:16:38 +0000

    Commit FRAGMENT FUNCIONAL -7

---
+++ SAPUI5-Expert-05_Estructura_Proyectos_SAPUI5/SAPUI5-Expert-05_Estructura_Proyectos_SAPUI5/Invoices/SAPUI5/webapp/controller/App.controller.js
@@ -0,0 +1,16 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller 
+     */
+    function (Controller) {
+        "use strict";
+
+        return Controller.extend("logaligroup.SAPUI5.controller.App", {
+
+            onInit: function () {
+
+            },
+        });
+    });
